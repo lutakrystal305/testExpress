1a830628fd290182701ea3a12b7e40ab63b71143
krystal <nguyenvanthai305@gmail.com>
2020-06-16 07:14:36 +0700

    All modification

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
 var express = require('express');
 var app = express();
-var port = 3005;
-app.get('/',(req, res) => res.send('What else?'));
-app.listen(port, () => console.log('Example app listening on port'+port));
+var bodyParser= require('body-parser');
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+
+ db= low(adapter);
+
+ db.defaults({ users: [] })
+  .write()
+
+var port= 3005;
+
+
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+app.get('/',function(req, res){
+ res.render('index',
+  {name: 'TH'}
+);
+});
+app.get('/users',function(req, res) {
+	res.render('users/index', {
+	users: db.get('users').value()
+});
+});
+app.get('/users/search', function(req, res){
+	var q=req.query.q;
+	var matchedUsers = users.filter(function(user){
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	res.render('users/index', {
+		users: matchedUsers
+	})
+
+});
+app.get('/users/create', function(req, res){
+	res.render('users/create');
+});
+app.post('/users/create', function(req, res){
+	db.get('users').push(req.body).write();
+	res.redirect('/users');
+})
+app.listen(port, function() {
+	console.log('Example app listening on port'+port);
+});
