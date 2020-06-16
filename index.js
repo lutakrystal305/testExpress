c659890a40f56bf98c1589bada1d9147ed14335f
krystal <nguyenvanthai305@gmail.com>
2020-06-16 20:14:40 +0700

    Testing view id users

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bodyParser= require('body-parser');
 var low = require('lowdb');
 var FileSync = require('lowdb/adapters/FileSync');
 var adapter = new FileSync('db.json');
+var shortid = require('shortid');
 
  db= low(adapter);
 
@@ -33,6 +34,7 @@ app.get('/users',function(req, res) {
 });
 app.get('/users/search', function(req, res){
 	var q=req.query.q;
+	var users= db.get('users').value();
 	var matchedUsers = users.filter(function(user){
 		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
 	});
@@ -44,7 +46,15 @@ app.get('/users/search', function(req, res){
 app.get('/users/create', function(req, res){
 	res.render('users/create');
 });
+app.get('/users/:id', function(req,res) {
+	var id= req.params.id;
+	user= db.get('users').find({id: id}).value();
+	res.render('users/view', {
+		user: user
+	});
+});
 app.post('/users/create', function(req, res){
+	req.body.id = shortid.generate();
 	db.get('users').push(req.body).write();
 	res.redirect('/users');
 })
