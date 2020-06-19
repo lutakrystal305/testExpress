db7560cdba8fda79814630d47070a35bd78a01cf
krystal <nguyenvanthai305@gmail.com>
2020-06-19 07:57:27 +0700

    Validation

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -21,6 +21,22 @@ module.exports.create = function(req, res){
 };
 module.exports.postCreate = function(req, res){
 	req.body.id = shortid.generate();
+	var errors= [];
+
+	if (!req.body.name) {
+		errors.push('Name is required.');
+	}
+	if (!req.body.date) {
+		errors.push('Date is required.');
+	}
+	if (errors.length) {
+		res.render('users/create', {
+			errors: errors,
+			values: req.body
+		});
+		return;
+	}
+
 	db.get('users').push(req.body).write();
 	res.redirect('/users');
 }
