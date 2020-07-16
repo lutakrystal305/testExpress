7f899f6bf4af97a2f6fa075ce3a465fdda8d8eaa
krystal <nguyenvanthai305@gmail.com>
2020-07-16 12:57:04 +0700

    zzz

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,41 +1,44 @@
 var Product= require('../models/product.model');
 
-module.exports.index= async function(req, res) {
-	var products = await Product.find();
-	var count = await Product.countDocuments();
-	var page= parseInt(req.query.page)|| 1;
-	var perPage= 8;
-	var begin= (page-1)*perPage;
+module.exports.index= async function(req, res, next) {
+	try {var products = await Product.find();
+		var count = await Product.countDocuments();
+		var page= parseInt(req.query.page)|| 1;
+		var perPage= 8;
+		var begin= (page-1)*perPage;
 
-	var nextPage={};
-	var prePage= {};
+		var nextPage={};
+		var prePage= {};
 
-	nextPage.url='/products?page='+(page+1);
-	nextPage.number= page+1;
+		nextPage.url='/products?page='+(page+1);
+		nextPage.number= page+1;
 
-	prePage.url='/products?page='+(page-1);
-	prePage.number= page-1;
+		prePage.url='/products?page='+(page-1);
+		prePage.number= page-1;
 
-	if ((count % perPage) !==0) {
-		endPage= parseInt(Math.floor(count/perPage)+1);
-	}
-	else {
-		endPage=count/perPage;
-	}
-
-
-	var start= (page-1)*perPage;
-	var end= page*perPage;
-	var productsN=await Product.find().limit(perPage).skip(begin);
-	res.render('products/index', {
-		products: productsN,
-		currentPage: page,
-		nextPage: nextPage,
-		prePage: prePage,
-		topUrl: '/products?page=1',
-		endPage: {
-			url: '/products?page=' + endPage,
-			number: endPage
+		if ((count % perPage) !==0) {
+			endPage= parseInt(Math.floor(count/perPage)+1);
 		}
-	})
+		else {
+			endPage=count/perPage;
+		}
+
+
+		var start= (page-1)*perPage;
+		var end= page*perPage;
+		var productsN=await Product.find().limit(perPage).skip(begin);
+		res.render('products/index', {
+			products: productsN,
+			currentPage: page,
+			nextPage: nextPage,
+			prePage: prePage,
+			topUrl: '/products?page=1',
+			endPage: {
+				url: '/products?page=' + endPage,
+				number: endPage
+			}
+		})
+	} catch (error) {
+		next(error);
+	}
 };
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var csurf = require('csurf');
 var mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb+srv://lutakrystal:vanthai305@cluster0.vx61n.mongodb.net/test');
+mongoose.connect('mongodb+srv://lutakrystal:vanthai305@cluster0.vx61n.mongodb.net/express-demo');
 
 
 var usersRoute= require('./Route/users.route');
