ceff97470a3fe8fb2d74284366f92f84f38e8bbc
krystal <nguyenvanthai305@gmail.com>
2020-07-16 13:46:23 +0700

    zzzzzzzzz

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,49 +1,53 @@
 var Product= require('../models/product.model');
 
 module.exports.index= async function(req, res, next) {
-	try {/*
+	try {
 		var products = await Product.find();
-		var count = await Product.countDocuments();
-		var page= parseInt(req.query.page)|| 1;
-		var perPage= 8;
-		var begin= (page-1)*perPage;
-
-		var nextPage={};
-		var prePage= {};
-
-		nextPage.url='/products?page='+(page+1);
-		nextPage.number= page+1;
-
-		prePage.url='/products?page='+(page-1);
-		prePage.number= page-1;
-
-		if ((count % perPage) !==0) {
-			endPage= parseInt(Math.floor(count/perPage)+1);
-		}
-		else {
-			endPage=count/perPage;
-		}
-
-
-		var start= (page-1)*perPage;
-		var end= page*perPage;
-		var productsN=await Product.find().limit(perPage).skip(begin);
-		res.render('products/index', {
-			products: productsN,
-			currentPage: page,
-			nextPage: nextPage,
-			prePage: prePage,
-			topUrl: '/products?page=1',
-			endPage: {
-				url: '/products?page=' + endPage,
-				number: endPage
-			}
-		})*/
-		var products = await Product.find();
-		res.render('products/index', {
-		products: products
-		});
 	} catch (error) {
 		next(error);
 	}
+	try {
+		var count = await Product.countDocuments();
+	} catch (error) {
+		next(error);
+	}
+	var page= parseInt(req.query.page)|| 1;
+	var perPage= 8;
+	var begin= (page-1)*perPage;
+
+	var nextPage={};
+	var prePage= {};
+
+	nextPage.url='/products?page='+(page+1);
+	nextPage.number= page+1;
+
+	prePage.url='/products?page='+(page-1);
+	prePage.number= page-1;
+
+	if ((count % perPage) !==0) {
+		endPage= parseInt(Math.floor(count/perPage)+1);
+	}
+	else {
+		endPage=count/perPage;
+	}
+
+
+	var start= (page-1)*perPage;
+	var end= page*perPage;
+	try {
+		var productsN=await Product.find().limit(perPage).skip(begin);
+	} catch (error) {
+		next(error);
+	}
+	res.render('products/index', {
+		products: productsN,
+		currentPage: page,
+		nextPage: nextPage,
+		prePage: prePage,
+		topUrl: '/products?page=1',
+		endPage: {
+			url: '/products?page=' + endPage,
+			number: endPage
+		}
+	})
 };
