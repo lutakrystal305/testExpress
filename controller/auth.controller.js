f8be59503a3840193a80b4aab9437f994a6d5a80
krystal <nguyenvanthai305@gmail.com>
2020-07-17 11:10:19 +0700

    12123

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -8,34 +8,35 @@ module.exports.login= function (req, res) {
 	res.render('auth/login');
 }
 module.exports.postLogin= async function(req, res, next) {
-	var email = req.body.email;
-	var password = req.body.password;
+	try {	
+		var email = req.body.email;
+		var password = req.body.password;
 
-	var user = await User.findOne({ email: email });
-	if (!user) {
-		res.render('auth/login', {
-			errors: [
-			'User does not exist!'
-			], 	
-			values: req.body
+		var user = await User.findOne({ email: email });
+		if (!user) {
+			res.render('auth/login', {
+				errors: [
+				'User does not exist!'
+				], 	
+				values: req.body
+			});
+			return;
+		}
+		var hashedPassword= md5(password);
+		if (user.password !== hashedPassword) {
+			res.render('auth/login', {
+				errors: [
+				'Password is wrong, you can try again!'
+				],
+				values: req.body 
+			});
+			return;
+		}
+		res.cookie('userID', user._id, {
+			signed: true
 		});
-		return;
+		res.redirect('/users');
+	} catch (error) {
+		next(error);
 	}
-	var hashedPassword= md5(password);
-	if (user.password !== hashedPassword) {
-		res.render('auth/login', {
-			errors: [
-			'Password is wrong, you can try again!'
-			],
-			values: req.body 
-		});
-		return;
-	}
-	res.cookie('userID', user._id, {
-		signed: true
-	});
-	res.redirect('/users');
 };
-postLogin().catch(function(error) {
-	console.error(error);
-})
--- controller/users.controller.js
+++ controller/users.controller.js
@@ -2,47 +2,51 @@ var User= require('../models/user.model');
 
 
 module.exports.index = async function(req, res, next) {
-	var users=await User.find();
-	res.render('users/index', {
-	users: users
-	});
+	try {
+		var users=await User.find();
+		res.render('users/index', {
+		users: users
+		});
+	} catch(error) {
+		next(error);
+	}
 }
-index().catch(function(error) {
-	console.error(error);
-})
 module.exports.search = async function(req, res, next){
-	var q=req.query.q;
-	var matchedUsers =await User.find().filter(function(user){
-		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-	});
-	res.render('users/index', {
-		users: matchedUsers
-	})
+	try {
+		var q=req.query.q;
+		var matchedUsers =await User.find().filter(function(user){
+			return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+		});
+		res.render('users/index', {
+			users: matchedUsers
+		})
+	} catch(error) {
+		next(error);
+	}
 }
-search().catch(function(error) {
-	console.error(error);
-})
 module.exports.create = function(req, res){
 	console.log(req.cookies);
 	res.render('users/create');
 
 };
 module.exports.postCreate =async function(req, res, next){
-	req.body.avatar = req.file.path.split('\\').slice(1).join('/');
-	var user=await new User(req.body);
-	user.save();
-	res.redirect('/users');
+	try {
+		req.body.avatar = req.file.path.split('\\').slice(1).join('/');
+		var user=await new User(req.body);
+		user.save();
+		res.redirect('/users');
+	} catch (error) {
+		next(error);
+	}
 }
-postCreate().catch(function(error) {
-	console.error(error);
-})
 module.exports.view = async function(req, res, next) {
-	var id= req.params.id;
-	user= await User.findOne({_id: id});
-	res.render('users/view', {
-		user: user
-	});
+	try {
+		var id= req.params.id;
+		user= await User.findOne({_id: id});
+		res.render('users/view', {
+			user: user
+		});
+	} catch(error) {
+		next(error);
+	}
 };
-view().catch(function(error) {
-	console.error(error);
-})
